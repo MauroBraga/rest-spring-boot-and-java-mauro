d41ff73d38202c6cfa4af2ed6d52429b1ecded43
mauro <mrb0305@gmail.com>
2023-11-13 22:02:09 -0300

    Tratando exceptions

--- src/main/java/br/com/mrb/restspringbootandjavamauro/controller/MathController.java
+++ src/main/java/br/com/mrb/restspringbootandjavamauro/controller/MathController.java
@@ -1,10 +1,14 @@
 package br.com.mrb.restspringbootandjavamauro.controller;
 
+import br.com.mrb.restspringbootandjavamauro.converter.NumberConverter;
 import br.com.mrb.restspringbootandjavamauro.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import static br.com.mrb.restspringbootandjavamauro.converter.NumberConverter.convertToDouble;
+import static br.com.mrb.restspringbootandjavamauro.converter.NumberConverter.isNumeric;
 
 
 @RestController
@@ -22,17 +26,66 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
-    private Double convertToDouble(String strNumber) {
-        if(strNumber==null) return 0D;
-        var number = strNumber.replaceAll(",",".");
-        if(isNumeric(number)) return Double.parseDouble(number);
-        return 0D;
+
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}",
+            method=RequestMethod.GET)
+    public Double subtraction(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a value numeric");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
     }
 
-    private boolean isNumeric(String strNumber) {
-        if(strNumber==null) return false;
-        var number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}",
+            method=RequestMethod.GET)
+    public Double multiplication(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a value numeric");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}",
+            method=RequestMethod.GET)
+    public Double division(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a value numeric");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+
+    @RequestMapping(value = "/mean/{numberOne}/{numberTwo}",
+            method=RequestMethod.GET)
+    public Double mean(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a value numeric");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) /2;
+    }
+
+    @RequestMapping(value = "/squareRoot/{number}",
+            method=RequestMethod.GET)
+    public Double squareRoot(
+            @PathVariable(value = "number") String number
+    ) throws Exception{
+
+        if(!isNumeric(number)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return Math.sqrt(convertToDouble(number));
     }
 
 }
---
+++ src/main/java/br/com/mrb/restspringbootandjavamauro/converter/NumberConverter.java
@@ -0,0 +1,16 @@
+package br.com.mrb.restspringbootandjavamauro.converter;
+
+public class NumberConverter {
+    public static Double convertToDouble(String strNumber) {
+        if(strNumber==null) return 0D;
+        var number = strNumber.replaceAll(",",".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if(strNumber==null) return false;
+        var number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
