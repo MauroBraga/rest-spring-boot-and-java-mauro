2abd87593bc8bebf4dfa2e5e57a9f7609c8cc64c
mauro <mrb0305@gmail.com>
2023-11-15 14:36:04 -0300

    Teste Repository

--- src/test/java/br/com/mrb/restspringbootandjavamauro/PersonRepositoryTest.java
+++ src/test/java/br/com/mrb/restspringbootandjavamauro/PersonRepositoryTest.java
@@ -9,19 +9,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 @DataJpaTest
 public class PersonRepositoryTest {
 
     @Autowired
     private PersonRepository personRepository;
+
+    Person person ;
+
     @BeforeEach
-    public void setup() {}
+    public void setup() {
+        person =  new Person("Mauro","Braga","Rio de Janeiro - Brasil","Male","mrb0305@gmail.com");
+    }
 
     @DisplayName("JUnit test for Given Person Object when Save then Return Saved Person")
     @Test
     void testGivenPersonObject_whenSave_thenReturnSavedPerson() {
         //Given / Arange
-        var person = new Person("Mauro","Braga","Rio de Janeiro - Brasil","Male","mrb0305@gmail.com");
+
         //When / Act
         var personSaved = personRepository.save(person);
 
@@ -34,7 +41,6 @@ public class PersonRepositoryTest {
     @Test
     void testGivenPersonList_whenFindAll_thenReturnPersonList() {
         //Given / Arange
-        var person = new Person("Mauro","Braga","Rio de Janeiro - Brasil","Male","mrb0305@gmail.com");
         var person1 = new Person("Oscar","Braga","Rio de Janeiro - Brasil","Male","oscar@gmail.com");
 
         personRepository.save(person);
@@ -52,12 +58,53 @@ public class PersonRepositoryTest {
     @Test
     void testGivenPersonObject_whenFindByID_thenReturnPersonObject() {
         //Given / Arange
-        var person = new Person("Mauro","Braga","Rio de Janeiro - Brasil","Male","mrb0305@gmail.com");
         personRepository.save(person);
         //When / Act
         var personSaved = personRepository.findById(person.getId()).get();
         //Then / Assert
         assertNotNull(personSaved);
         assertEquals(person.getId(),personSaved.getId());
+    }
+
+    @DisplayName("JUnit test for Given Person Object when findByEmail then Return Person Object")
+    @Test
+    void testGivenPersonObject_whenFindByEmail_thenReturnPersonObject() {
+        //Given / Arange
+        personRepository.save(person);
+        //When / Act
+        var personSaved = personRepository.findByEmail(person.getEmail()).get();
+        //Then / Assert
+        assertNotNull(personSaved);
+        assertEquals(person.getId(),personSaved.getId());
+    }
+
+    @DisplayName("JUnit test for Given Person Object when Update Person then Return Updated Person Object")
+    @Test
+    void testGivenPersonObject_whenUpdatePerson_thenReturnUpdatedPersonObject() {
+        //Given / Arange
+        personRepository.save(person);
+        //When / Act
+        var personSaved = personRepository.findById(person.getId()).get();
+        personSaved.setFirstName("Leonardo");
+        personSaved.setEmail("leonardo@erudio.com.br");
+
+        var updatedPerson = personRepository.save(personSaved);
+
+        //Then / Assert
+        assertNotNull(updatedPerson);
+        assertEquals("Leonardo", updatedPerson.getFirstName());
+        assertEquals("leonardo@erudio.com.br", updatedPerson.getEmail());
+    }
+
+    @DisplayName("JUnit test for Given Person Object when Delete then Remove Person")
+    @Test
+    void testGivenPersonObject_whenDelete_thenRemovePerson(){
+        //Given / Arange
+        personRepository.save(person);
+        //When / Act
+        personRepository.deleteById(person.getId());
+        Optional<Person> personOptional = personRepository.findById(person.getId());
+        //Then / Assert
+        assertTrue(personOptional.isEmpty());
     }
 }
