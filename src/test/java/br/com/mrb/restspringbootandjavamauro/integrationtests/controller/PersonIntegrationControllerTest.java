b2b628183a26bcaaff08e17e751700684629f557
mauro <mrb0305@gmail.com>
2023-11-19 18:35:52 -0300

    Teste Integracao

--- src/test/java/br/com/mrb/restspringbootandjavamauro/integrationtests/controller/PersonIntegrationControllerTest.java
+++ src/test/java/br/com/mrb/restspringbootandjavamauro/integrationtests/controller/PersonIntegrationControllerTest.java
@@ -94,5 +94,158 @@ public class PersonIntegrationControllerTest extends AbstractIntegrationTest {
 
     }
 
+    @Test
+    @Order(2)
+    @DisplayName("JUnit integration given Person Object when Update one Person should Return a Updated Person Object")
+    void integrationTestGivenPersonObject_when_UpdateOnePerson_ShouldReturnAUpdatedPersonObject() throws JsonMappingException, JsonProcessingException  {
+
+        person.setFirstName("Leonardo");
+        person.setEmail("leonardo@erudio.com.br");
+
+        // Given / Arrange
+        var content = given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .put()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        Person updatedPerson = objectMapper.readValue(content, Person.class);
+
+        person = updatedPerson;
+
+        assertNotNull(updatedPerson);
+
+        assertNotNull(updatedPerson.getId());
+        assertNotNull(updatedPerson.getFirstName());
+        assertNotNull(updatedPerson.getLastName());
+        assertNotNull(updatedPerson.getAddress());
+        assertNotNull(updatedPerson.getGender());
+        assertNotNull(updatedPerson.getEmail());
+
+        assertTrue(updatedPerson.getId() > 0);
+        assertEquals("Leonardo", updatedPerson.getFirstName());
+        assertEquals("Braga", updatedPerson.getLastName());
+        assertEquals("Rio de Janeiro - Brasil", updatedPerson.getAddress());
+        assertEquals("Male", updatedPerson.getGender());
+        assertEquals("leonardo@erudio.com.br", updatedPerson.getEmail());
+
+    }
+
+
+    @Test
+    @Order(3)
+    @DisplayName("JUnit integration given Person Object when findById should Return a Person Object")
+    void integrationTestGivenPersonObject_when_findById_ShouldReturnAPersonObject() throws JsonMappingException, JsonProcessingException {
+
+        var content = given().spec(specification)
+                .pathParam("id", person.getId())
+                .when()
+                .get("{id}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        Person foundPerson = objectMapper.readValue(content, Person.class);
+
+        assertNotNull(foundPerson);
+
+        assertNotNull(foundPerson.getId());
+        assertNotNull(foundPerson.getFirstName());
+        assertNotNull(foundPerson.getLastName());
+        assertNotNull(foundPerson.getAddress());
+        assertNotNull(foundPerson.getGender());
+        assertNotNull(foundPerson.getEmail());
+
+        assertTrue(foundPerson.getId() > 0);
+        assertEquals("Leonardo", foundPerson.getFirstName());
+        assertEquals("Braga", foundPerson.getLastName());
+        assertEquals("Rio de Janeiro - Brasil", foundPerson.getAddress());
+        assertEquals("Male", foundPerson.getGender());
+        assertEquals("leonardo@erudio.com.br", foundPerson.getEmail());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("JUnit integration given Person Object when findAll should Return a Persons List")
+    void integrationTest_when_findAll_ShouldReturnAPersonsList() throws JsonMappingException, JsonProcessingException {
+
+        Person anotherPerson =  new Person("Gabriela","Rodriguez","Uberlândia - Minas Gerais - Brasil","Female","gabi@erudio.com.br");
+
+        given().spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(anotherPerson)
+                .when()
+                .post()
+                .then()
+                .statusCode(200);
+
+        var content = given().spec(specification)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        Person[] myArray = objectMapper.readValue(content, Person[].class);
+        List<Person> people = Arrays.asList(myArray);
+
+        Person foundPersonOne = people.get(0);
+
+        assertNotNull(foundPersonOne);
+
+        assertNotNull(foundPersonOne.getId());
+        assertNotNull(foundPersonOne.getFirstName());
+        assertNotNull(foundPersonOne.getLastName());
+        assertNotNull(foundPersonOne.getAddress());
+        assertNotNull(foundPersonOne.getGender());
+        assertNotNull(foundPersonOne.getEmail());
+
+        assertTrue(foundPersonOne.getId() > 0);
+        assertEquals("Leonardo", foundPersonOne.getFirstName());
+        assertEquals("Braga", foundPersonOne.getLastName());
+        assertEquals("Rio de Janeiro - Brasil", foundPersonOne.getAddress());
+        assertEquals("Male", foundPersonOne.getGender());
+        assertEquals("leonardo@erudio.com.br", foundPersonOne.getEmail());
+
+        Person foundPersonTwo = people.get(1);
+
+        assertNotNull(foundPersonTwo);
+
+        assertNotNull(foundPersonTwo.getId());
+        assertNotNull(foundPersonTwo.getFirstName());
+        assertNotNull(foundPersonTwo.getLastName());
+        assertNotNull(foundPersonTwo.getAddress());
+        assertNotNull(foundPersonTwo.getGender());
+        assertNotNull(foundPersonTwo.getEmail());
+
+        assertTrue(foundPersonTwo.getId() > 0);
+        assertEquals("Gabriela", foundPersonTwo.getFirstName());
+        assertEquals("Rodriguez", foundPersonTwo.getLastName());
+        assertEquals("Uberlândia - Minas Gerais - Brasil", foundPersonTwo.getAddress());
+        assertEquals("Female", foundPersonTwo.getGender());
+        assertEquals("gabi@erudio.com.br", foundPersonTwo.getEmail());
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("JUnit integration given Person Object when delete should Return No Content")
+    void integrationTestGivenPersonObject_when_delete_ShouldReturnNoContent() throws JsonMappingException, JsonProcessingException {
+
+        given().spec(specification)
+                .pathParam("id", person.getId())
+                .when()
+                .delete("{id}")
+                .then()
+                .statusCode(204);
+    }
 
 }
